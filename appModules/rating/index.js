851e1242ece51d824d4fd73e27c94f1bea4a8d09
kittyest <nad91346289@gmail.com>
2024-04-18 18:44:25 +0300

    :)

--- appModules/rating/index.js
+++ appModules/rating/index.js
@@ -1,8 +1,12 @@
 const config = require('./config');
 const makeRatingFile = require('./rating-file');
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const createRating = require('./calculations');
 const updateRating = require('./calculations');
+=======
+const { createRating, updateRating } = require('./calculations');
+>>>>>>> Stashed changes
 =======
 const { createRating, updateRating } = require('./calculations');
 >>>>>>> Stashed changes
--- controllers/vote.js
+++ controllers/vote.js
@@ -1,10 +1,14 @@
 const fs = require("fs").promises;
 const parseBody = require("../appModules/http-utils/parse-body");
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const { config, createRating, updateRating } = require("../appModules/rating");
 =======
 const { createRating, updateRating } = require("../appModules/rating");
 const config = require("../appModules/rating");
+>>>>>>> Stashed changes
+=======
+const { config, createRating, updateRating } = require("../appModules/rating");
 >>>>>>> Stashed changes
 
 async function voteRouteController(req, res) {
